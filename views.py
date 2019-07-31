64604f096c256c8a9b7a6dcba81d2ba00f811f9d
ChoiHanwool <48754156+ChoiHanwool@users.noreply.github.com>
2019-07-31 16:11:57 +0900

    Add files via upload

---
+++ views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+import random
+
+# Create your views here.
+def start(request):
+    return render(request, 'start.html')
+
+def result(request):
+    name = request.GET['name']
+    spoon = [1, 2, 3, 4, 5]
+    result = ['못생김', '매력', '수명', '재력', '예쁨', '귀여움']
+    random.shuffle(spoon)
+    random.shuffle(result)
+    return render(request, 'result.html', {'name':name, 'spoon':spoon, 'result':result })
+    
